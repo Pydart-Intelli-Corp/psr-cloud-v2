9c9b6658b2b764f4487d771d36455d9ed2c808ef
Pydart-Intelli-Corp <info.pydart@gmail.com>
2026-02-09 13:14:53 +0530

    feat: Load environment variables from .env file in database configuration

--- config/database.js
+++ config/database.js
@@ -1,4 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/no-require-imports
+require('dotenv').config();
+// eslint-disable-next-line @typescript-eslint/no-require-imports
 const path = require('path');
 
 module.exports = {
